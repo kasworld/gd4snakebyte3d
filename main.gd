bc9abbd3c9158c899917ae87eb649c2c5d57a524
SeukWon Kang <kasworld@gmail.com>
2025-04-02 15:32:27 +0900

    adjust camera light

--- main.gd
+++ main.gd
@@ -4,6 +4,6 @@ extends Node3D
 
 func _ready() -> void:
 	var vp_size = get_viewport().get_visible_rect().size
-	$Camera3D.position = Vector3(1,1,64)
-	$Camera3D.look_at(Vector3.ZERO)
-	$OmniLight3D.position = Vector3(1,1,32)
+	$Camera3D.position = Vector3(Settings.FieldWidth/2,Settings.FieldHeight/2,Settings.FieldWidth)
+	$Camera3D.look_at(Vector3(Settings.FieldWidth/2,Settings.FieldHeight/2,0))
+	$OmniLight3D.position = Vector3(Settings.FieldWidth/2,Settings.FieldHeight/2,Settings.FieldWidth)
